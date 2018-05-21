ab635ebbc9acc1fa453b8006471163db006f0e02
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-05-21 08:52:46 +0700

    Add POST /todos route and refractor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User',{
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    name: {
+        type: String
+    }
+});
+
+module.exports = {User};
